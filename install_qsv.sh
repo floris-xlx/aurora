18819d8d6d45954c871a2062cb0b3315fea8c7d2
floris-xlx <floris@xylex.ai>
2025-02-21 05:23:40 +0100

    Update .gitignore and remove unused CSV parser module

---
+++ install_qsv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -O - https://dathere.github.io/qsv-deb-releases/qsv-deb.gpg | sudo gpg --dearmor -o /usr/share/keyrings/qsv-deb.gpg
+echo "deb [signed-by=/usr/share/keyrings/qsv-deb.gpg] https://dathere.github.io/qsv-deb-releases ./" | sudo tee /etc/apt/sources.list.d/qsv.list
+sudo apt update
+sudo apt install qsv
