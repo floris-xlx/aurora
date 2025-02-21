998564c6b34e59ae7d6bf4e669952b4ae4d22808
Floris <floris@xylex.ai>
2025-02-21 01:03:57 +0100

    added api  mod.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use supabase_rs::tests::methods::init;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
+
 fn main() {
+    init_tracing();
     println!("Hello, world!");
+}
+
+
+/// ## Initialize Tracing
+/// 
+/// This function sets up the tracing subscriber for logging and monitoring.
+/// 
+/// ### Example
+/// 
+/// ```
+/// init_tracing();
+/// ```
+fn init_tracing() {
+    let filter: EnvFilter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+
+    tracing_subscriber::fmt().with_env_filter(filter).init()
 }
